3df12a8774c746d09f31cdec30b1652b4d17eea3
amoghsarpotdar <amogh.a.sarpotdar@gmail.com>
2017-08-08 14:57:28 +0100

    Adapter pattern implementation.

---
+++ Adapter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target target = new Target();
+            target.Request();
+
+
+            TargetAdapter targetAdapter = new TargetAdapter();
+            targetAdapter.Request();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Adapter/TargetAdapter.cs
@@ -0,0 +1,12 @@
+﻿namespace Adapter
+{
+    class TargetAdapter : Target
+    {
+        private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            _adaptee.SpecificRequest();
+        }
+    }
+}
