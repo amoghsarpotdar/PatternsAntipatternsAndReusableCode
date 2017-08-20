b3e334c33466c27bbe3f875c30bfd192b4ed8d26
amoghsarpotdar <amogh.a.sarpotdar@gmail.com>
2017-08-20 20:29:39 +0100

    InsertionSort Implementatin

--- SortingAlgos/CArray.cs
+++ SortingAlgos/CArray.cs
@@ -57,5 +57,23 @@ namespace SortingAlgos
                 DisplayElements();
             }
         }
+
+        public void SelectionSort()
+        {
+            int min, temp;
+            for (int outer = 0; outer <= upper; outer++)
+            {
+                min = outer;
+                for (int inner = outer + 1; inner <= upper; inner++)
+                {
+                    if (arr[inner] < arr[min])
+                        min = inner;
+
+                    temp = arr[outer];
+                    arr[outer] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+        }
     }
 }
