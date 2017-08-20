c88753511c0a1055d886571573caab7d475063c9
amoghsarpotdar <amogh.a.sarpotdar@gmail.com>
2017-08-20 19:46:20 +0100

    BubbleSort implementation

---
+++ SortingAlgos/CArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SortingAlgos
+{
+    class CArray
+    {
+        private int[] arr;
+        private int upper;
+        private int _numElements;
+
+        public CArray(int size)
+        {
+            arr = new int[size];
+            upper = size - 1;
+            _numElements = 0;
+        }
+
+        public void Insert(int item)
+        {
+            arr[_numElements] = item;
+            _numElements++;
+        }
+
+        public void DisplayElements()
+        {
+            Console.WriteLine("--------");
+            for (int i = 0; i <= upper; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("--------");
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i <= upper; i++)
+            {
+                arr[i] = 0;
+            }
+            _numElements = 0;
+        }
+        
+        public void BubbleSort()
+        {
+            int temp;
+            for (int outer = upper; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner <= outer - 1; inner++)
+                {
+                    if (arr[inner] > arr[inner + 1])
+                    {
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+                    }
+                }
+                DisplayElements();
+            }
+        }
+    }
+}
---
+++ SortingAlgos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SortingAlgos
+{
+    class Program
+    {
+        static void Main()
+        {
+            CArray nums = new CArray(12);
+            Random rnd = new Random(11);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                nums.Insert((int)(rnd.NextDouble()*100));
+            }
+            Console.WriteLine("Beffore sorting: ");
+            nums.DisplayElements();
+            Console.WriteLine("During sorting: ");
+            nums.BubbleSort();
+            Console.WriteLine("After sorting: ");
+            nums.DisplayElements();
+            Console.ReadKey();
+        }
+    }
+}
